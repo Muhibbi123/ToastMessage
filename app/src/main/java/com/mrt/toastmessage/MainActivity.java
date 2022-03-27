30fa43d2df34d15052cce215014f39478cfd7e0a
Muhibbi123 <mert_ertok@hotmail.com>
2022-03-27 19:28:44 +0300

    Initial commit

---
+++ app/src/main/java/com/mrt/toastmessage/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mrt.toastmessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),
+                        "Did you see that toast? :D",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
